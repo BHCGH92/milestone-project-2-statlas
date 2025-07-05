337571c086668f045d5bc8f78edec45c1df0a08d
BHCGH92 <ben.holt1992@live.co.uk>
2025-07-05 08:22:58 +0100

    added displayFlag, clearGameDisplay, toggleGameControls and updateScoreDisplay functions into flaggame.js

--- scripts/flaggame.js
+++ scripts/flaggame.js
@@ -22,4 +22,46 @@ function toggleFlagGameLoader(visible) {
     currentflagImg.classList.toggle('d-none', visible);
 };
 
+/** JSDOc comment - function displayFlag
+ * Display the flag image in the img element.
+ * @param {string} flagPng - The URL of the flag image to display.
+ * @param {string} flatAlt - The alt text for the flag image.
+ */
+function displayFlag(flagPng, flagAlt) {
+    current.FlagImg.src = flagPng;
+    currentFlagImg.alt = flagAlt;
+    currentFlagImg.classList.remove('d-none');
+};
 
+/**
+ * clears the game area (input, message area etc.)
+ */
+function clearGameDisplay() {
+    gameMessageDiv.innerHTML = '';
+    countryGuessInput.value = '';
+    /* Removes bootstrap validation styling */
+    countryGuessInput.classList.remove('is-invalid', 'is-valid');
+};
+
+/** JSDOC comment - function toggleGameControls
+ * Toggles the visibility and enabled/disabled state of the guess input and submit button.
+ * @param {boolean} enable- If true, shows the guess input and submit button; otherwise hides them.
+ */
+function toggleGameControls(enable) {
+    if (enable) {
+        guessSection.classList.remove('d-none');
+        countryGuessInput.removeAttribute('disabled');
+        submitGuessButton.removeAttribute('disabled');
+    } else {
+        guessSection.classList.add('d-none');
+        countryGuessInput.setAttribute('disabled', 'true');
+        submitGuessButton.setAttribute('disabled', 'true');
+    }
+};
+
+/**
+ * upadate the score the score
+ */
+function updateScoreDisplay() {
+    currentScore.textContent = score;
+};
